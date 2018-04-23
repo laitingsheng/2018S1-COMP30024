957e9a841a1ac305f14b95f1b69886812484895e
Tinson Lai <laitinson@gmail.com>
2018-04-23 14:24:06 +1000

    deal with forfeit move

--- Player.py
+++ Player.py
@@ -46,6 +46,7 @@ class Player:
     def _move(self, turns):
         board = self.board
         alpha = -inf
+        s = None
         for src, dests in board.valid_move(self.mine):
             for dest in dests:
                 b = board.copy()
@@ -55,6 +56,10 @@ class Player:
                     alpha = re
                     s, d = src, dest
 
+        # forfeit move
+        if s is None:
+            return None
+
         self.board.move(*s, *d)
         return s, d
 
@@ -62,6 +67,9 @@ class Player:
         if depth == self.depth or board.end():
             return self._eval_move(board, turns)
 
+        depth += 1
+        turns += 1
+        updated = False
         for src, dests in board.valid_move(self.mine):
             for dest in dests:
                 b = board.copy()
@@ -69,14 +77,23 @@ class Player:
                 re = self._move_min(board, depth, turns, alpha, beta)
                 if re > alpha:
                     alpha = re
+                    updated = True
                     if alpha >= beta:
                         return beta
+
+        # forfeit move
+        if not updated:
+            return self._move_min(board, depth, turns, alpha, beta)
+
         return alpha
 
     def _move_min(self, board, depth, turns, alpha, beta):
         if depth == self.depth or board.end():
             return self._eval_move(board, turns)
 
+        depth += 1
+        turns += 1
+        updated = False
         for src, dests in board.valid_move(self.oppo):
             for dest in dests:
                 b = board.copy()
@@ -84,8 +101,14 @@ class Player:
                 re = self._move_max(board, depth, turns, alpha, beta)
                 if re < beta:
                     beta = re
+                    updated = True
                     if beta <= alpha:
                         return beta
+
+        # forfeit move
+        if not updated:
+            return self._move_min(board, depth, turns, alpha, beta)
+
         return beta
 
     def _place(self):
@@ -103,11 +126,12 @@ class Player:
 
     def _place_max(self, board, depth, alpha, beta):
         if board.count[self.mine] == 12:
-            return self._move_max(board, depth, 0, alpha, beta)
+            return self._move_max(board, depth + 1, 0, alpha, beta)
 
         if depth == self.depth:
             return self._eval_place(board)
 
+        depth += 1
         for pos in board.valid_place(self.mine):
             b = board.copy()
             b.place(pos, self.mine)
@@ -120,11 +144,12 @@ class Player:
 
     def _place_min(self, board, depth, alpha, beta):
         if board.count[self.oppo] == 12:
-            return self._move_min(board, depth, 0, alpha, beta)
+            return self._move_min(board, depth + 1, 0, alpha, beta)
 
         if depth == self.depth:
             return self._eval_place(board)
 
+        depth += 1
         for pos in board.valid_place(self.mine):
             b = board.copy()
             b.place(pos, self.mine)
